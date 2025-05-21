5597455faaec2cc8f3f5bb98b80344a356f2cde1
Radek Halfar <hal0068@vsb.cz>
2025-05-21 13:27:43 +0200

    added UNet network

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 from train import train_model
 from model_CNN_test import KappaPredictorCNN
+from model_UNet_test import UNet3D
 import torch.nn as nn
 
 if __name__ == '__main__':
@@ -11,7 +12,13 @@ if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f"Using device: {device}")
     
-    model = KappaPredictorCNN(in_channels=12).to(device)
+    #model = KappaPredictorCNN(in_channels=12).to(device)
+    model = UNet3D(
+        in_channels=12,  # Number of input variables in KappaDataset
+        n_classes=2,    # kappa_SU and kappa_CA
+        bilinear=True   # Use bilinear upsampling (smoother but potentially less sharp)
+    ).to(device)
+    
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     loss_fn = nn.MSELoss()
     
--- model_UNet_test.py
+++ model_UNet_test.py
@@ -2,65 +2,119 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class DoubleConv(nn.Module):
-    def __init__(self, in_ch, out_ch):
+class DoubleConv3D(nn.Module):
+    """(3D convolution => [BN] => ReLU) * 2"""
+    def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+        if not mid_channels:
+            mid_channels = out_channels
+            
+        self.double_conv = nn.Sequential(
+            nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm3d(mid_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm3d(out_channels),
             nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
-        return self.conv(x)
+        return self.double_conv(x)
 
-class UNetEncoder(nn.Module):
-    def __init__(self, in_ch, base_ch=64):
+class Down3D(nn.Module):
+    """Downscaling with maxpool then double conv"""
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.enc1 = DoubleConv(in_ch, base_ch)
-        self.enc2 = DoubleConv(base_ch, base_ch * 2)
-        self.pool = nn.MaxPool2d(2)
-
-    def forward(self, x):
-        x1 = self.enc1(x)        # [B, C, H, W]
-        x2 = self.pool(x1)
-        x2 = self.enc2(x2)
-        return x2  # Bottleneck features per time step
-
-class TemporalUNet(nn.Module):
-    def __init__(self, in_ch=12, out_ch=2, hidden_dim=128):
-        super().__init__()
-        self.encoder = UNetEncoder(in_ch)
-        
-        # Assuming bottleneck gives [B, C, H/4, W/4]
-        self.temporal_lstm = nn.LSTM(input_size=hidden_dim,
-                                     hidden_size=hidden_dim,
-                                     batch_first=True)
-
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(hidden_dim, 64, kernel_size=2, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, out_ch, kernel_size=3, padding=1)
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool3d(2),
+            DoubleConv3D(in_channels, out_channels)
         )
 
-    def forward(self, x):  # x: [B, T, C, H, W]
-        B, T, C, H, W = x.shape
-        feats = []
+    def forward(self, x):
+        return self.maxpool_conv(x)
 
-        for t in range(T):
-            enc = self.encoder(x[:, t])  # [B, hidden_dim, H', W']
-            feats.append(enc.flatten(2)) # Flatten spatial dims
+class Up3D(nn.Module):
+    """Upscaling then double conv"""
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+        
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
+            self.conv = DoubleConv3D(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose3d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv3D(in_channels, out_channels)
 
-        feats = torch.stack(feats, dim=1)  # [B, T, hidden_dim * H' * W']
-        feats = feats.mean(-1)  # [B, T, hidden_dim] as simplification
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        
+        # Handle potential size mismatches due to padding
+        diffZ = x2.size()[2] - x1.size()[2]
+        diffY = x2.size()[3] - x1.size()[3]
+        diffX = x2.size()[4] - x1.size()[4]
+        
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2,
+                        diffZ // 2, diffZ - diffZ // 2])
+        
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
 
-        lstm_out, _ = self.temporal_lstm(feats)  # [B, T, hidden_dim]
-        last = lstm_out[:, -1]  # Use last timestep
+class OutConv3D(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv3D, self).__init__()
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=1)
 
-        # Reshape back to spatial (H', W')
-        B, H_, W_ = B, H//4, W//4
-        spatial_feat = last.view(B, -1, H_, W_)  # [B, hidden_dim, H', W']
+    def forward(self, x):
+        return self.conv(x)
 
-        out = self.decoder(spatial_feat)
-        return out  # [B, out_ch, H, W]
+class UNet3D(nn.Module):
+    def __init__(self, in_channels=12, n_classes=2, bilinear=False):
+        super(UNet3D, self).__init__()
+        self.in_channels = in_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        # Downsampling path (encoder)
+        self.inc = DoubleConv3D(in_channels, 32)
+        self.down1 = Down3D(32, 64)
+        self.down2 = Down3D(64, 128)
+        self.down3 = Down3D(128, 256)
+        
+        # Bottleneck
+        factor = 2 if bilinear else 1
+        self.down4 = Down3D(256, 512 // factor)
+        
+        # Upsampling path (decoder) - adjust output channels to match depth
+        self.up1 = Up3D(512, 256 // factor, bilinear)
+        self.up2 = Up3D(256, 128 // factor, bilinear)
+        self.up3 = Up3D(128, 64 // factor, bilinear)
+        self.up4 = Up3D(64, 32, bilinear)
+        
+        # Output convolution - now outputs 3D predictions
+        self.outc = OutConv3D(32, n_classes)
+        
+        # Optional: Add dropout for regularization
+        self.dropout = nn.Dropout3d(0.2)
+
+    def forward(self, x):
+        # Encoder
+        x1 = self.inc(x)       # Initial double conv
+        x2 = self.down1(x1)    # Downsample 1
+        x3 = self.down2(x2)    # Downsample 2
+        x4 = self.down3(x3)    # Downsample 3
+        x5 = self.down4(x4)    # Bottleneck
+        
+        # Apply dropout in the bottleneck
+        x5 = self.dropout(x5)
+        
+        # Decoder with skip connections
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        
+        # Final 3D output
+        logits = self.outc(x)  # [batch_size, n_classes, depth, height, width]
+        
+        return logits
